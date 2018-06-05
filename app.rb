1b246a0a4451eae6e69ac5b4c15b97b5bb8bfdc4
Mirko Strugar <mirko@byte-brothers.com>
2018-06-05 12:29:13 +0200

    Implement basic JSON routes

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ require 'dotenv'
 class Coordinator < Sinatra::Base
   Dotenv.load
 
+  before do
+    content_type :json
+  end
+  get '/search' do
+    unless params[:address]
+      halt 400, {error: 'Please provide an address to look for.'}.to_json
+    end
+  end
+  after do
+    JSON.dump(response.body)
+  end
   run! if app_file == $0
 end
