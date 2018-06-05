c7352b13a9a3853b2d8b2732522fe83a0c4efe56
Mirko Strugar <mirko@byte-brothers.com>
2018-06-05 15:57:54 +0200

    Add super low-level ‚authentication‘

--- app.rb
+++ app.rb
@@ -1,18 +1,26 @@
 require 'sinatra/base'
 require 'dotenv'
+require 'pry'
 
 require_relative 'adapters/adapter'
 
 class Coordinator < Sinatra::Base
   Dotenv.load
 
+  mockup_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
   # API
   get '/search' do
-    unless params[:address]
-      error = {errors: [message: 'Please provide a geocodable search query (e.g. an address)']}
-      halt 400, {'Content-Type' => 'application/json'}, error.to_json
+    if env['HTTP_AUTHORIZATION'] && env['HTTP_AUTHORIZATION'].split(':').last.strip == mockup_token
+      unless params[:address]
+        error = {errors: [message: 'Please provide a geocodable search query (e.g. an address)']}
+        halt 400, {'Content-Type' => 'application/json'}, error.to_json
+      else
+        Adapter.new(params[:address], params[:provider]).get_coordinates
+      end
     else
-      Adapter.new(params[:address], params[:provider]).get_coordinates
+      error = {errors: [message: 'You are not authorized to use this API.']}
+      halt 401, {'Content-Type' => 'application/json'}, error.to_json
     end
   end
 
